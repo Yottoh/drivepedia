a2874a0df6e3f15ae5f655f564cae9d618ad5efe
Yottoh <105390849+Yottoh@users.noreply.github.com>
2025-03-26 04:00:05 +0200

    Delete PROEKT directory

--- PROEKT/project folder/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start(); // Започва сесията
-session_unset(); // Изчиства всички сесийни променливи
-session_destroy(); // Разрушава сесията
-
-// Пренасочване към главната страница (index.php)
-header("Location: autoinfo.php");
-exit();
-?>
